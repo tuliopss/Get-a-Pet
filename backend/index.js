f74cea6da8825f8df11d0b83ab786900c64511a7
unknown <tuliovictorp@hotmail.com>
2023-08-18 19:13:44 -0300

    controllers and models

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+
+const UserRoutes = require("./routes/UserRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+//Cors
+app.use(cors({ credentials: true, origin: "http//localhost:3000" }));
+
+app.use(express.static("public"));
+
+//Routes
+app.use("/user", UserRoutes);
+
+app.listen(5000);
