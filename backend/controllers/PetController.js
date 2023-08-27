e066f0be2f79cabd1124683a34e8928448b2bcc3
unknown <tuliovictorp@hotmail.com>
2023-08-27 16:13:36 -0300

    finished backend

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -251,10 +251,42 @@ module.exports = class PetController {
     };
 
     await Pet.findByIdAndUpdate(id, pet);
+    res.status(200).json({
+      message: `A visita foi agendada com sucesso. Entre em contato com ${pet.user.name} pelo telefone ${pet.user.phone}`,
+    });
+  }
+
+  static async concludeAdoption(req, res) {
+    const id = req.params.id;
+
+    //id validatins to find a pet
+    if (!ObjectId.isValid(id)) {
+      res.status(422).json({ message: "ID inválido" });
+      return;
+    }
+
+    const pet = await Pet.findOne({ _id: id });
+    if (!pet) {
+      res.status(404).json({ message: "Pet não encontrado." });
+      return;
+    }
+
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    if (pet.user.id.toString() !== user._id.toString()) {
+      res
+        .status(422)
+        .json({ message: "Houve um problema em processar sua solicitação!" });
+      return;
+    }
+
+    pet.available = false;
+
+    await Pet.findByIdAndUpdate(id, pet);
+
     res
       .status(200)
-      .json({
-        message: `A visita foi agendada com sucesso. Entre em contato com ${pet.user.name} pelo telefone ${pet.user.phone}`,
-      });
+      .json({ message: "Parabéns! Adoção concluida com sucesso!" });
   }
 };
