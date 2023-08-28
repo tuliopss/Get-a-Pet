6f14571a29c4b7ab285aff031b2c729ca8ddb7fd
unknown <tuliovictorp@hotmail.com>
2023-08-28 18:16:28 -0300

    hook useAuth register

--- frontend/src/components/pages/Auth/Register.jsx
+++ frontend/src/components/pages/Auth/Register.jsx
@@ -1,44 +1,58 @@
 import Input from "../../form/Input";
 import styles from "../../form/Form.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Register = () => {
+  const [user, setUser] = useState({});
+
   const handleChange = (e) => {
-    e.preventDefault();
+    setUser({ ...user, [e.target.name]: e.target.value });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
   return (
     <section className={styles.form_container}>
       <h1>Registrar</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Input
           text='Nome'
           type='text'
           name='name'
           placeholder='Digite seu nome'
+          handleOnChange={handleChange}
         />
         <Input
           text='Telefone'
           type='text'
           name='phone'
           placeholder='Digite seu telefone'
+          handleOnChange={handleChange}
         />
         <Input
           text='Email'
           type='email'
           name='email'
           placeholder='Digite seu email'
+          handleOnChange={handleChange}
         />
         <Input
           text='Senha'
           type='password'
           name='password'
           placeholder='Digite sua senha'
+          handleOnChange={handleChange}
         />
         <Input
           text='Confirme sua senha'
           type='password'
           name='confirmPassword'
           placeholder='Confirme sua senha'
+          handleOnChange={handleChange}
         />
 
         <input type='submit' value='Cadastrar' />
