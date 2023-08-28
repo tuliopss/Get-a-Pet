5c350eb4e61efb96032eee9c416f2b47679c7c01
unknown <tuliovictorp@hotmail.com>
2023-08-28 16:02:09 -0300

    register form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,23 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./components/pages/Auth/Login";
 import Register from "./components/pages/Auth/Register";
 import Home from "./components/pages/Home";
 import Navbar from "./components/layouts/Navbar";
 import Footer from "./components/layouts/Footer";
+import Container from "./components/layouts/Container";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        <Route path='/register'>
-          <Register />
-        </Route>
-
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
+      <Container>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Container>
       <Footer />
     </Router>
   );
--- frontend/src/components/pages/Auth/Register.jsx
+++ frontend/src/components/pages/Auth/Register.jsx
@@ -1,7 +1,51 @@
+import Input from "../../form/Input";
+import styles from "../../form/Form.module.css";
+import { Link } from "react-router-dom";
+
 const Register = () => {
+  const handleChange = (e) => {
+    e.preventDefault();
+  };
   return (
-    <section>
-      <h1>Register</h1>
+    <section className={styles.form_container}>
+      <h1>Registrar</h1>
+      <form>
+        <Input
+          text='Nome'
+          type='text'
+          name='name'
+          placeholder='Digite seu nome'
+        />
+        <Input
+          text='Telefone'
+          type='text'
+          name='phone'
+          placeholder='Digite seu telefone'
+        />
+        <Input
+          text='Email'
+          type='email'
+          name='email'
+          placeholder='Digite seu email'
+        />
+        <Input
+          text='Senha'
+          type='password'
+          name='password'
+          placeholder='Digite sua senha'
+        />
+        <Input
+          text='Confirme sua senha'
+          type='password'
+          name='confirmPassword'
+          placeholder='Confirme sua senha'
+        />
+
+        <input type='submit' value='Cadastrar' />
+      </form>
+      <p>
+        Já possui uma conta? <Link to='/login'>Clique aqui</Link>
+      </p>
     </section>
   );
 };
